91386a334a48bef6fb954f2957ca59594558d6ff
nurmeden <nurmeden.02@gmail.com>
2023-05-31 22:43:12 +0500

    Сreating the customer entity

--- shared/models/customer.go
+++ shared/models/customer.go
@@ -1,8 +1,8 @@
 package models
 
 type Customer struct {
-	ID      int64  `json:"id"`
-	Name    string `json:"name"`
-	Email   string `json:"email"`
-	Address string `json:"address"`
+	ID     int64  `json:"id"`
+	Name   string `json:"name"`
+	Email  string `json:"email"`
+	Adress string `json:"address"`
 }
