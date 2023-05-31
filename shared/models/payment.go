0b7ceea35caa0cee669c61f8dfb0ecaa5bc0e546
nurmeden <nurmeden.02@gmail.com>
2023-05-31 23:04:58 +0500

    Сreating the payment entity

--- shared/models/payment.go
+++ shared/models/payment.go
@@ -1,7 +1,7 @@
 package models
 
 type Payment struct {
-	Id     int64   `json:"id"`
+	ID     int64   `json:"id"`
 	Amount float64 `json:"amount"`
 	Status string  `json:"status"`
 	Method string  `json:"method"`
