e89afb048aebe48db8cfc90a4deb0839477e467c
nurmeden <nurmeden.02@gmail.com>
2023-06-01 17:59:16 +0500

    Add configuration loading from environment variables

--- payment-service/config/configs.go
+++ payment-service/config/configs.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+)
+
 type Config struct {
 	Database DatabaseConfig
 	Redis    RedisConfig
@@ -18,5 +23,25 @@ type RedisConfig struct {
 }
 
 func LoadConfig() (Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return Config{}, err
+	}
 
+	viper.BindEnv("Database.Host", "DATABASE_HOST")
+	viper.BindEnv("Database.Port", "DATABASE_PORT")
+	viper.BindEnv("Database.User", "DATABASE_USER")
+	viper.BindEnv("Database.Password", "DATABASE_PASSWORD")
+	viper.BindEnv("Database.Name", "DATABASE_NAME")
+
+	viper.BindEnv("Redis.Host", "REDIS_HOST")
+	viper.BindEnv("Redis.Port", "REDIS_PORT")
+
+	var cfg Config
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
 }
