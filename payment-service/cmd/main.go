523655ed38f18137cdf48fde80d67603616f73dd
nurmeden <nurmeden.02@gmail.com>
2023-06-01 22:48:11 +0500

    feat: Add main function

---
+++ payment-service/cmd/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+
+	"github.com/labstack/echo"
+	"github.com/nurmeden/PaymentGateway/payment-service/config"
+	"github.com/nurmeden/PaymentGateway/payment-service/pkg/cache"
+	"github.com/nurmeden/PaymentGateway/payment-service/pkg/database"
+)
+
+func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+	}
+	db, err := database.InitDatabase(cfg.Database)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	redisClient := cache.InitRedisClient(cfg.Redis)
+
+	e := echo.New()
+
+	paymentRepository := repositories.NewPaymentRepository(db)
+	paymentService := services.NewPaymentService(paymentRepository, redisClient)
+	paymentHandler := handlers.NewPaymentHandler(paymentService)
+
+	e.POST("/payments", paymentHandler.CreatePayment)
+	e.GET("/payments/:id", paymentHandler.GetPaymentByID)
+	e.PUT("/payments/:id", paymentHandler.UpdatePayment)
+	e.DELETE("/payments/:id", paymentHandler.DeletePayment)
+
+	port := cfg.Port
+	if port == "" {
+		port = "8080"
+	}
+	e.Logger.Fatal(e.Start(":" + port))
+}
