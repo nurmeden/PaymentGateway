b76fd718ef6eb7cd527728d4eef11fd282e5518f
nurmeden <nurmeden.02@gmail.com>
2023-06-01 22:59:46 +0500

    feat: Add payment repository

---
+++ payment-service/api/payment/repositories/payment_repository.go
@@ -0,0 +1,16 @@
+package repositories
+
+import "database/sql"
+
+type PaymentRepository interface {
+}
+
+type paymentRepository struct {
+	db *sql.DB
+}
+
+func NewPaymentRepository(db *sql.DB) PaymentRepository {
+	return &paymentRepository{
+		db: db,
+	}
+}
