9452bbdabad109965c9eb72a3bdf413d1fb3b96f
nurmeden <nurmeden.02@gmail.com>
2023-06-02 16:45:29 +0500

    feat: Add Create method to PaymentRepository

--- payment-service/api/payment/repositories/payment_repository.go
+++ payment-service/api/payment/repositories/payment_repository.go
@@ -1,8 +1,17 @@
 package repositories
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+
+	"github.com/nurmeden/PaymentGateway/shared/models"
+)
 
 type PaymentRepository interface {
+	CreatePayment(payment *models.Payment) error
+	// GetPaymentById(id string) (models.Payment, error)
+	// UpdatePayment(payment *models.Payment) error
+	// DeletePayment(id string) error
 }
 
 type paymentRepository struct {
@@ -13,4 +22,17 @@ func NewPaymentRepository(db *sql.DB) PaymentRepository {
 	return &paymentRepository{
 		db: db,
 	}
+}
+
+func (h *paymentRepository) CreatePayment(payment *models.Payment) error {
+	query := `
+		INSERT INTO payments (amount, status, method)
+		VALUES ($2, $3, $4)
+	`
+	err := h.db.QueryRow(query, payment.Amount, payment.Status, payment.Method)
+	if err != nil {
+		return errors.New("error queryRow")
+	}
+
+	return nil
 }
