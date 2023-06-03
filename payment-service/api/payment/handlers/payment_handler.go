f15c1d7bd4882bae4d3d2d3710324be85c3d44da
nurmeden <nurmeden.02@gmail.com>
2023-06-03 17:39:09 +0500

    fix: Convert a string id to an int64

--- payment-service/api/payment/handlers/payment_handler.go
+++ payment-service/api/payment/handlers/payment_handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/nurmeden/PaymentGateway/payment-service/api/payment/services"
@@ -34,12 +35,12 @@ func (h *paymentHandler) CreatePayment(c echo.Context) error {
 }
 
 func (h *paymentHandler) GetPaymentByID(c echo.Context) error {
-	payment := new(models.Payment)
-	if err := c.Bind(payment); err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
+	paramId := c.Param("id")
+	id, err := strconv.ParseInt(paramId, 10, 64)
+	if err != nil {
+		panic(err)
 	}
-
-	payment, err := h.paymentService.GetPaymentByID(payment.ID)
+	payment, err := h.paymentService.GetPaymentByID(id)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
