ce26985a490b4d6be2aba6f4034e0dda2c0ded7c
nurmeden <nurmeden.02@gmail.com>
2023-06-05 15:39:24 +0500

    feat: Add create method to customerHandler

--- customer-service/api/customer/handlers/customer.handlers.go
+++ customer-service/api/customer/handlers/customer.handlers.go
@@ -1,13 +1,34 @@
 package handlers
 
-import customerServices "github.com/nurmeden/PaymentGateway/customer-service/api/customer/services"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	customerServices "github.com/nurmeden/PaymentGateway/customer-service/api/customer/services"
+	"github.com/nurmeden/PaymentGateway/shared/models"
+)
 
 type customerHandler struct {
-	customerService *customerServices.CustomerService
+	customerService customerServices.CustomerService
 }
 
-func NewCustomerHandler(customerServices customerServices.CustomerService) *customerHandler {
-	return &customerHandler{
-		customerService: &customerServices,
+func NewCustomerHandler(customerServices customerServices.CustomerService) customerHandler {
+	return customerHandler{
+		customerService: customerServices,
 	}
+}
+
+func (h *customerHandler) CreateCustomer(c echo.Context) error {
+
+	customer := new(models.Customer)
+	if err := c.Bind(customer); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	err := h.customerService.CreateCustomer(customer)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, customer)
 }
