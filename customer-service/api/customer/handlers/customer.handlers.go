508095a3715898cec512bbc0e1911fe20ae4431e
nurmeden <nurmeden.02@gmail.com>
2023-06-05 15:08:17 +0500

    feat: Add create method to customerRepository

--- customer-service/api/customer/handlers/customer.handlers.go
+++ customer-service/api/customer/handlers/customer.handlers.go
@@ -1,32 +1,13 @@
 package handlers
 
-import (
-	"net/http"
-
-	"github.com/labstack/echo"
-	customerServices "github.com/nurmeden/PaymentGateway/customer-service/api/customer/services"
-	"github.com/nurmeden/PaymentGateway/shared/models"
-)
+import customerServices "github.com/nurmeden/PaymentGateway/customer-service/api/customer/services"
 
 type customerHandler struct {
-	customerService customerServices.CustomerService
+	customerService *customerServices.CustomerService
 }
 
 func NewCustomerHandler(customerServices customerServices.CustomerService) *customerHandler {
 	return &customerHandler{
-		customerService: customerServices,
+		customerService: &customerServices,
 	}
-}
-
-func (h *customerHandler) CreateCustomer(c echo.Context) error {
-	customer := new(models.Customer)
-	if err := c.Bind(customer); err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
-	}
-
-	err := h.customerService.CreateCustomer(customer)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, err.Error())
-	}
-	return c.JSON(http.StatusOK, customer)
 }
