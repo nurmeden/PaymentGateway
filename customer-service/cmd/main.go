9888d97931b5e862e453d63f2e9d9a6206a3e099
nurmeden <nurmeden.02@gmail.com>
2023-06-05 18:31:27 +0500

    feat: Create handler instances

--- customer-service/cmd/main.go
+++ customer-service/cmd/main.go
@@ -4,9 +4,9 @@ import (
 	"log"
 
 	"github.com/labstack/echo"
+	"github.com/nurmeden/PaymentGateway/customer-service/api/customer/handlers"
 	"github.com/nurmeden/PaymentGateway/customer-service/api/customer/repositories"
-	"github.com/nurmeden/PaymentGateway/customer-service/api/customer/services"
-	"github.com/nurmeden/PaymentGateway/customer-service/api/payment/services"
+	customerServices "github.com/nurmeden/PaymentGateway/customer-service/api/customer/services"
 	"github.com/nurmeden/PaymentGateway/customer-service/config"
 	"github.com/nurmeden/PaymentGateway/customer-service/pkg/database"
 )
@@ -26,5 +26,7 @@ func main() {
 	defer db.Close()
 
 	customerRepo := repositories.NewCustomerRepository(db)
-	customerService := services.NewCustomerService(customerRepo)
+	customerService := customerServices.NewCustomerService(customerRepo)
+	customerHandler := handlers.NewCustomerHandler(customerService)
+
 }
