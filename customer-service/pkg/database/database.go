7beda6d3fb5a4f19b23d723729aaf0c99d8b99a8
nurmeden <nurmeden.02@gmail.com>
2023-06-05 18:16:55 +0500

    feat: Add function InitDatabase in customer-service

---
+++ customer-service/pkg/database/database.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/nurmeden/PaymentGateway/customer-service/config"
+)
+
+func InitDatabase(dbConfig config.DatabaseConfig) (*sql.DB, error) {
+	dbURI := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Name)
+
+	db, err := sql.Open("postgres", dbURI)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
