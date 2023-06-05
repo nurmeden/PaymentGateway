05919662d67e02a3e7c356a59ea1dbcbb09ac852
nurmeden <nurmeden.02@gmail.com>
2023-06-05 15:35:38 +0500

    feat: Add create method to customerService

--- customer-service/api/customer/services/customer_service.go
+++ customer-service/api/customer/services/customer_service.go
@@ -1,16 +1,29 @@
 package customerServices
 
-import "github.com/nurmeden/PaymentGateway/customer-service/api/customer/repositories"
+import (
+	"github.com/nurmeden/PaymentGateway/customer-service/api/customer/repositories"
+	"github.com/nurmeden/PaymentGateway/shared/models"
+)
 
 type CustomerService interface {
+	CreateCustomer(customer *models.Customer) error
 }
 
 type customerService struct {
-	customerRepository *repositories.CustomerRepository
+	customerRepository repositories.CustomerRepository
 }
 
-func NewCustomerService(customerRepository *repositories.CustomerRepository) *customerService {
+func NewCustomerService(customerRepository repositories.CustomerRepository) *customerService {
 	return &customerService{
 		customerRepository: customerRepository,
 	}
+}
+
+func (s *customerService) CreateCustomer(customer *models.Customer) error {
+	err := s.customerRepository.CreateCustomer(customer)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
