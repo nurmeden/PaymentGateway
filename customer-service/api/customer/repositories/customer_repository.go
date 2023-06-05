2f7356c4b20d866e8ac86442f635ab94bd7ad400
nurmeden <nurmeden.02@gmail.com>
2023-06-05 15:12:29 +0500

    Revert "Revert "feat: Add create method to customerRepository""
    This reverts commit 7764e251c8532a46b9de33475ded4246cf1245f7.

--- customer-service/api/customer/repositories/customer_repository.go
+++ customer-service/api/customer/repositories/customer_repository.go
@@ -1,13 +1,8 @@
 package repositories
 
-import (
-	"database/sql"
-
-	"github.com/nurmeden/PaymentGateway/shared/models"
-)
+import "database/sql"
 
 type CustomerRepository interface {
-	CreateCustomer(customer *models.Customer) error
 }
 
 type customerRepository struct {
@@ -18,16 +13,4 @@ func NewCustomerRepository(db *sql.DB) *customerRepository {
 	return &customerRepository{
 		db: db,
 	}
-}
-
-func (r *customerRepository) CreateCustomer(customer *models.Customer) error {
-	query := `INSERT INTO customers(name,email,phone)
-	          VALUES ($1, $2, $3)`
-
-	_, err := r.db.Exec(query, customer.Name, customer.Email, customer.Phone)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
