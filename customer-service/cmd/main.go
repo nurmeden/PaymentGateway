ecef1f2ac48278d8834b0fa724e9f76ebccd1c6e
nurmeden <nurmeden.02@gmail.com>
2023-06-05 18:25:55 +0500

    feat: Create a new Echo instance

--- customer-service/cmd/main.go
+++ customer-service/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/labstack/echo"
 	"github.com/nurmeden/PaymentGateway/customer-service/config"
 )
 
@@ -11,4 +12,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to load configuration: %s", err.Error())
 	}
+
+	e := echo.New()
+
 }
